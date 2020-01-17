72a17f31ba6ece4e285257fcfd28fe189d9f5557
conbec <56614504+conbec@users.noreply.github.com>
2020-01-17 09:00:51 -0800

    flat file working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
+
 function App() {
+
+  const [numbers, getNumbers] = useState([1, 2, 3]);
+  
+  const evens = numbers.filter(even => even % 2 === 0);
+  const odds = numbers.filter(odd => odd % 2 !== 0);
+
+  const addRandom = () => {
+    getNumbers([...numbers, Math.ceil(Math.random() * 100)])
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>The Numbers Game</h1>
+        <h4>We have {numbers.length} numbers</h4>
+        <button onClick={() => { 
+          addRandom();
+          console.log(numbers)
+        }}>Get More Numbers</button>
+        <div className="numberLists">
+          <h5>All Numbers</h5>
+          <ul>
+            {numbers.map((number, idx) => {
+              return (
+                <li key={idx}>{number}</li>
+              )
+            })}
+          </ul>
+          <h5>Even Numbers</h5>
+          <ul>
+            {evens.map((number, idx) => {
+              return (<li key={idx}>{number}</li>)
+            })}
+          </ul>
+          <h5>Odd Numbers</h5>
+          <ul>
+            {odds.map((number, idx) => {
+              return (<li key={idx}>{number}</li>)
+            })}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
